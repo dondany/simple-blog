1ccaecdb9dfa8fb8a9fff110964464289b4214a1
Daniel <danielzietala@gmail.com>
2021-05-13 05:21:13 -0700

    Updated testdata

--- backend/testdata/sample_data.sql
+++ backend/testdata/sample_data.sql
@@ -1,13 +1,17 @@
+DROP TABLE IF EXISTS posts;
 CREATE TABLE posts
 (
     id SERIAL NOT NULL,
 	title varchar(200) not null,
-    content varchar(2000) NOT NULL
+    content varchar(2000) NOT NULL,
+    likes int default 0
 )
 
-insert into posts (title, content) values ('Second Post', 'Wow, it is the second post already!');
-insert into posts (title, content) values ('Third Post', 'Ok, third one is here!');
+insert into posts (title, content, likes) values ('First Post', 'Yo!', 1);
+insert into posts (title, content, likes) values ('Second Post', 'Wow, it is the second post already!', 5);
+insert into posts (title, content, likes) values ('Third Post', 'Ok, third one is here!', 12);
 
+DROP TABLE IF EXISTS comments;
 CREATE TABLE comments
 (
     id SERIAL NOT NULL,
